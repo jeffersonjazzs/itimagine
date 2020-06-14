2273317862cdcfb416910b013f84e6bf1a231b26
jeffersonjazzs <jefferson_jazzs@hotmail.com>
2020-06-14 16:51:27 -0300

    init

---
+++ src/main/java/com/br/itimagine/ItimagineApplication.java
@@ -0,0 +1,13 @@
+package com.br.itimagine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItimagineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItimagineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/itimagine/ItimagineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.itimagine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItimagineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
